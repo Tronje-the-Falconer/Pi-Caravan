3b65abc56e13d8cdb1add91fe8d0050528c22fa7
Tronje-the-Falconer <club@kording-web.de>
2018-08-23 11:22:11 +0200

    Update pi_caravan_init.py

--- opt/pi-caravan/pi_caravan_init.py
+++ opt/pi-caravan/pi_caravan_init.py
@@ -8,9 +8,9 @@
     setting up inital settings
 """
 
-import pi_caravan_1wire_sensors
+import pi_caravan_init_onewire_sensors
 
 def get_sensors():
-    temperature_sensor_designation, temperature_senor_count, temperature_sensor_value = pi_caravan_1wire_sensors.import_temperature_sensors()
+    temperature_sensor_designation, temperature_senor_count, temperature_sensor_value = pi_caravan_init_onewire_sensors.import_temperature_sensors()
 
 loopcounter = 0                      #  Zaehlt die Durchlaeufe des Mainloops
