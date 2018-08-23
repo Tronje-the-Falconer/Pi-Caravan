69a98f5a5218a14297c332819493b6c30faa0936
Tronje-the-Falconer <club@kording-web.de>
2018-08-23 09:19:20 +0200

    Update pi_caravan_names.py

--- opt/pi-caravan/pi_caravan_names.py
+++ opt/pi-caravan/pi_caravan_names.py
@@ -7,3 +7,15 @@
     
     setting up variables
 """
+
+
+# fields
+key_field = 'key'
+value_field = 'value'
+loglevel_file_field='loglevel_file'
+loglevel_console_field='loglevel_console'
+
+
+# tables
+debug_table = 'debug'
+
