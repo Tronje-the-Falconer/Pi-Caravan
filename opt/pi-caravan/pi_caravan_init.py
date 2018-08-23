79e3eca1be609b39feb7788b0fbbbdd84b32497c
Tronje-the-Falconer <club@kording-web.de>
2018-08-23 06:53:18 +0200

    Create pi_caravan_init.py

---
+++ opt/pi-caravan/pi_caravan_init.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# coding=utf-8
+# pi_caravan_init.py
+#------------------------------------------------------------
+"""
+    inital settings for pi-caravan
+    
+    setting up inital settings
+"""
