1a5dddec0f8eb624ea1e14102a8bab0b3d68af8b
Tronje-the-Falconer <club@kording-web.de>
2018-08-23 06:57:27 +0200

    Create pi_caravan_names.py

---
+++ opt/pi-caravan/pi_caravan_names.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# coding=utf-8
+# pi_caravan_names.py
+#------------------------------------------------------------#
+"""
+    pi-ager variables
+    
+    setting up variables
+"""
