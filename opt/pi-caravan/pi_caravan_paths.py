9046185cc9f6f54f2df90915f1155b01efa8bca2
Tronje-the-Falconer <club@kording-web.de>
2018-08-23 11:19:14 +0200

    Update pi_caravan_paths.py

--- opt/pi-caravan/pi_caravan_paths.py
+++ opt/pi-caravan/pi_caravan_paths.py
@@ -20,6 +20,7 @@ def set_paths():
     logfile_txt_file = '/var/www/logs/logfile.txt'
     pi_caravan_log_file = '/var/www/logs/pi-caravan.log'
     sqlite3_file = '/var/www/config/pi-ager.sqlite3'
+    onewire_path = '/sys/bus/w1/devices'
 
 def get_path_logfile_txt_file():
     """
@@ -44,3 +45,11 @@ def get_path_sqlite3_file():
     global sqlite3_file
     set_paths()
     return sqlite3_file
+
+ef get_path_onewire():
+    """
+    getting path for website logfile
+    """
+    global onewire_path
+    set_paths()
+    return onewire_path
