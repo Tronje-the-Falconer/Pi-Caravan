bab417cfad661ca8375b48454e05888c9586742f
Tronje-the-Falconer <club@kording-web.de>
2018-08-23 11:24:17 +0200

    Update pi_caravan_loop.py

--- opt/pi-caravan/pi_caravan_loop.py
+++ opt/pi-caravan/pi_caravan_loop.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 # pi_caravan_loop.py
 #------------------------------------------------------------
-import pi_caravan_temperature
+import pi_caravan_read_onewire_sensors
 
 def do_mainloop():
     global temperature_sensor_designation, temperature_senor_count, temperature_sensor_value 
@@ -10,7 +10,7 @@ def do_mainloop():
     try:
         while True:
             x = 0
-            pi_caravan_temperature.read_temperature_sensors()
+            pi_caravan_read_onewire_sensors.read_temperature_sensors()
             print ("Sensorbezeichnung und Temperaturwert:")
             while x < temperature_senor_count:
                 print temperature_sensor_designation[x] , " " , temperature_sensor_value[x] , " °C"
@@ -19,4 +19,4 @@ def do_mainloop():
             print ("\n")
     except:
         # error 
-        logger.warning('reading DS1820 sensors was not possible.')
+        logger.warning('reading temperature sensors was not possible.')
