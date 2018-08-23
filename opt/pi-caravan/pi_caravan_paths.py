914879df909f5bdeda19cf9a09c6d9e57e60532e
Tronje-the-Falconer <club@kording-web.de>
2018-08-23 08:09:15 +0200

    Create pi_caravan_paths.py

---
+++ opt/pi-caravan/pi_caravan_paths.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# coding=utf-8
+# pi_caravan_paths.py
+#------------------------------------------------------------
+"""
+    paths for pi-caravan
+    
+    setting up paths for pi-caravan
+"""
+
+def set_paths():
+    """
+    setting paths
+    """
+    
+    global logfile_txt_file
+    global pi_ager_log_file
+
+    logfile_txt_file = '/var/www/logs/logfile.txt'
+    pi_ager_log_file = '/var/www/logs/pi-ager.log'
+
+def get_path_logfile_txt_file():
+    """
+    getting path for website logfile
+    """
+    global logfile_txt_file
+    set_paths()
+    return logfile_txt_file
+    
+def get_pi_caravan_log_file_path():
+    """
+    getting path for logfile
+    """
+    global pi_ager_log_file
+    set_paths()
+    return pi_ager_log_file
