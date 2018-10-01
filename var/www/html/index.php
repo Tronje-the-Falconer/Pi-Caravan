32b0a1dfa646137d5b1e5f513908419565a6255d
Tronje-the-Falconer <club@kording-web.de>
2018-10-01 13:59:16 +0200

    Update index.php

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -6,21 +6,21 @@
 <form  method="post">
     <table style="width: 100%;">
         <tr>
-            <td><button class="art-button" name="Radio on">Radio on</button></td>
-            <td><button class="art-button" name="Radio off">Radio off</button></td>
+            <td><button class="art-button" name="radio_on">Radio on</button></td>
+            <td><button class="art-button" name="radio_off">Radio off</button></td>
         </tr>
         <tr>
-            <td><button class="art-button" name="Radio Volume up">Volume up</button></td>
-            <td><button class="art-button" name="Radio Volume down">Volume down</button></td>
+            <td><button class="art-button" name="radio_volume_up">Volume up</button></td>
+            <td><button class="art-button" name="radio_volume_down">Volume down</button></td>
         </tr>
         <tr>
-            <td><button class="art-button" name="Radio Seek Up">seek up</button></td>
-            <td><button class="art-button" name="Radio Seek Down">seek down</button></td>
+            <td><button class="art-button" name="radio_seek_up">seek up</button></td>
+            <td><button class="art-button" name="radio_seek_down">seek down</button></td>
             
         </tr>
         <tr>
-            <td><button class="art-button" name="Radio Tune">Tune</button> Tune Frequency:</td>
-            <td><input type="text" name="Radio Frequency"></td>
+            <td><button class="art-button" name="radio_tune">Tune</button> Tune Frequency:</td>
+            <td><input type="text" name="radio_frequency"></td>
         </tr>
         
     </table>
