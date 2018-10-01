b5f503170c9beed0d082d5fe5cd72ca4d1bed8e7
Tronje-the-Falconer <club@kording-web.de>
2018-10-01 10:20:59 +0200

    Create index.php

---
+++ var/www/html/index.php
@@ -0,0 +1,5 @@
+<?php
+    include 'header.php';                                       // Template-Kopf und Navigation                                
+    <h2 class="art-postheader">Pi Caravan</h2>
+?>
+
