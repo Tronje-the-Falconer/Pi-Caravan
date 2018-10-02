074640ab0ccdb4daa6c9fd6b5d88357ef087f5df
Tronje-the-Falconer <club@kording-web.de>
2018-10-02 09:53:45 +0200

    Update index.php

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -5,24 +5,26 @@
 <h2 class="art-postheader">Radio</h2>
 <form  method="post">
     <table style="width: 100%;">
-        <tr>
+               <tr>
             <td><button class="art-button" name="radio_on">Radio on</button></td>
             <td><button class="art-button" name="radio_off">Radio off</button></td>
+            <td></td>
         </tr>
         <tr>
             <td><button class="art-button" name="radio_volume_up">Volume up</button></td>
             <td><button class="art-button" name="radio_volume_down">Volume down</button></td>
+            <td></td>
         </tr>
         <tr>
             <td><button class="art-button" name="radio_seek_up">seek up</button></td>
             <td><button class="art-button" name="radio_seek_down">seek down</button></td>
-            
+            <td></td>
         </tr>
         <tr>
-            <td><button class="art-button" name="radio_tune">Tune</button> Tune Frequency:</td>
-            <td><input type="text" name="radio_frequency" type = "number"></td>
+            <td><button class="art-button" name="radio_tune">Tune</button></td>
+            <td>Tune Frequency:</td>
+            <td><input name="radio_frequency" type="number" maxlength="5" min="87.5" max="108" step="0.01" onchange="(function(el){el.value=parseFloat(el.value).toFixed(2);})(this)" placeholder="105,50"></td>
         </tr>
-        
     </table>
 </form>
 <?php
