a0b34485041692d4c5b96695f887bf2c8269c475
Tronje-the-Falconer <club@kording-web.de>
2018-10-04 21:09:01 +0200

    Add files via upload

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -4,10 +4,10 @@
 ?>
 <h1 class="art-postheader">Pi Caravan</h1>
 <h2 class="art-postheader">Radio</h2>
-<form  method="post">
+<form method="post">
     <table style="width: 20%;">
                <tr>
-            <td><button class="art-button" name="radio_on">Radio on</button></td>
+            <td><button class="art-button" name="radio_on" value="radio_on">Radio on</button></td>
             <td><button class="art-button" name="radio_reset">Radio on and reset</button></td>
             <td><button class="art-button" name="radio_off">Radio off</button></td>
         </tr>
--- var/www/html/modules/radio.php
+++ var/www/html/modules/radio.php
@@ -1,30 +1,28 @@
 <?php
     #
     if (isset($_POST['radio_on'])){
-        shell_exec('sudo /var/sudowebscript.sh radiostartstop up')
+        shell_exec('sudo /var/sudowebscript.sh radiostartstop up');
     }
     if (isset($_POST['radio_off'])){
-        shell_exec('sudo /var/sudowebscript.sh radiostartstop down')
+        shell_exec('sudo /var/sudowebscript.sh radiostartstop down');
     }
     if (isset($_POST['radio_reset'])){
-        shell_exec('sudo /var/sudowebscript.sh radioreset')
+        shell_exec('sudo /var/sudowebscript.sh radioreset');
     }
     if (isset($_POST['radio_volume_up'])){
-        shell_exec('sudo /var/sudowebscript.sh radiovolume 5') # Hardcoded! berechnen!
+        shell_exec('sudo /var/sudowebscript.sh radiovolume 10'); # Hardcoded! berechnen!
     }
     if (isset($_POST['radio_volume_down'])){
-        shell_exec('sudo /var/sudowebscript.sh radiovolume 2') # Hardcoded! berechnen!
+        shell_exec('sudo /var/sudowebscript.sh radiovolume 2'); # Hardcoded! berechnen!
     }
     if (isset($_POST['radio_seek_up'])){
-        shell_exec('sudo /var/sudowebscript.sh radioseek next')
-    }
+        shell_exec('sudo /var/sudowebscript.sh radioseek up');
     }
     if (isset($_POST['radio_seek_down'])){
-        shell_exec('sudo /var/sudowebscript.sh radioseek prev')
+        shell_exec('sudo /var/sudowebscript.sh radioseek down');
     }
+    $radiotune = 'sudo /var/sudowebscript.sh radiotunefreq ' . $_POST['radio_frequency'];
     if (isset($_POST['radio_tune'])){
-        shell_exec('sudo /var/sudowebscript.sh radiotunefreq $_POST['radio_frequency'])
+        shell_exec($radiotune);
     }
-    
-    
 ?>
